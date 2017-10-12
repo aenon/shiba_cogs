b21dcd3b7d8499d37c3addcb7d862567b4827bc2
Xilin Sun <s.sn.giraffe@gmail.com>
2017-10-11 18:41:13 -0700

    add code to handle special characters

--- fortune/fortune.py
+++ fortune/fortune.py
@@ -17,6 +17,7 @@ class fortune:
             fortune_text = subprocess.check_output(['fortune-zh'])
         fortune_text = fortune_text.decode('utf8')
         fortune_text = str(fortune_text).replace('\n', ' ').replace('\t', '  ')
+        fortune_text = str(fortune_text).replace('\x1b[32m', '').replace('\x1b[33m', '').replace('\x1b[m', '')
         await self.bot.say(fortune_text)
 
 
